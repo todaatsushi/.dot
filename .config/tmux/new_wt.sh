271717981ddf1c500cd41c8c4094b582be10af52
Atsushi Toda <todaatsushi0@gmail.com>
2022-02-10 19:50:41 +0000

    Improve scripte

--- .config/tmux/new_wt.sh
+++ .config/tmux/new_wt.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-source ~/.bash_wab
-
 if [ -z "$1" ]; then
     echo "No branch given"
     return
 fi
+
 
 new_branch="$1"
 session_name=$(tmux display-message -p "#S")
 clean_branch="$(echo "$new_branch" | tr "/" "__")"
 target="$session_name:$clean_branch"
 
+terminal-notifier -title "Build for $target started." -message "Branch name: $new_branch"
+
 if ! tmux has-session -t $target 2> /dev/null; then
     tmux neww -dn $clean_branch
 fi
 
-tmux send-keys -t "$target" new_wt " " "$new_branch" Enter `terminal-notifier -title "Build complete: $target" -subtitle "Branch name: $new_branch" --sound default` Enter
+source ~/.bash_wab
+tmux send-keys -t "$target" "new_wt $new_branch" Enter
 
