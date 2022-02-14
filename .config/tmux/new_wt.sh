8a67a44b7db47dfbaa9b281a9ac3dd2534f2a98b
Atsushi Toda <todaatsushi0@gmail.com>
2022-02-14 17:09:42 +0000

    Worktree migration

--- .config/tmux/new_wt.sh
+++ .config/tmux/new_wt.sh
@@ -10,11 +10,11 @@ session_name=$(tmux display-message -p "#S")
 clean_branch="$(echo "$new_branch" | tr "/" "__")"
 target="$session_name:$clean_branch"
 
-terminal-notifier -title "Build for $target started." -message "Branch name: $new_branch"
-
 if ! tmux has-session -t $target 2> /dev/null; then
     tmux neww -dn $clean_branch
 fi
+
+terminal-notifier -title "Build for $target started." -message "Branch name: $new_branch"
 
 source ~/.bash_wab
 tmux send-keys -t "$target" "new_wt $new_branch" Enter
--- .config/tmux/tmux-startup.sh
+++ .config/tmux/tmux-startup.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
+tmux new-session -s "wabsite" -n "database" -d
+tmux send-keys -t wabsite:database "cd ~/Workspace/web2/common" Enter
+tmux send-keys -t wabsite:database "docker compose up" Enter
 
-# Start work sesion
-tmux new-session -d -s "wabsite" -n "server"
-
-tmux send-keys -t wabsite:server "cdg website" Enter
-tmux send-keys -t wabsite:server "clear" Enter
-tmux send-keys -t wabsite:server "ddown" Enter
-tmux send-keys -t wabsite:server "badger_up" Enter
-
-tmux neww -n "terminal"
-tmux send-keys -t wabsite:terminal "cdg website" Enter
-tmux send-keys -t wabsite:terminal "sleep 5" Enter
-tmux send-keys -t wabsite:terminal "badger_local_image" Enter
-tmux send-keys -t wabsite:terminal "clear" Enter
-
-tmux neww -n "editor"
-tmux send-keys -t wabsite:editor "cdg website" Enter
-tmux send-keys -t wabsite:editor "nv" Enter
-
-tmux neww -n "ngrok"
-tmux send-keys -t wabsite:ngrok "cdg website" Enter
-tmux send-keys -t wabsite:ngrok "ng" Enter
-
-# start config session
-tmux new-session -d -s "configs" -n "config"
-tmux send-keys -t config:config "cd ~/.config/nvim" Enter
-tmux split-window -h
-tmux send-keys -t config:config "cd ~/.config/tmux" Enter
-
-tmux attach -t wabsite:editor
-
+tmux neww -n "staging"
+tmux send-keys -t wabsite:staging "cd ~/Workspace/web2/staging" Enter
