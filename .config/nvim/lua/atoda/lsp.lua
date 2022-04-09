367d2ced64a098d2d55cf160cab3799c28ca446f
Atsushi Toda <todaatsushi0@gmail.com>
2022-04-09 18:19:15 +0100

    Add more lsp commands

--- .config/nvim/lua/atoda/lsp.lua
+++ .config/nvim/lua/atoda/lsp.lua
@@ -10,7 +10,16 @@ local custom_on_attach = function(client)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
     vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
     vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer=0})
-    vim.keymap.set("n", "rr", vim.lsp.buf.rename, {buffer=0})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+
+    -- Actions
+    vim.keymap.set("n", "ca", vim.lsp.buf.rename, {buffer=0})
+
+    -- Diagnostic
+    vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set("n", "<leader>db", vim.diagnostic.goto_prev, {buffer=0})
+    -- Probs doesn't belong here
+    vim.keymap.set("n", "<leader>dl", "<CMD>Telescope diagnostics<CR>", {buffer=0})
 end
 
 local lspconfig = require('lspconfig')
