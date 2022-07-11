f19be0a6bce5f9f19782954222a5eb7e3de13fff
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-11 18:37:10 +0100

    move typescript sets to lua

---
+++ .config/nvim/ftplugin/typescript.lua
@@ -0,0 +1,16 @@
+local set = vim.opt
+
+set.relativenumber = true
+set.hidden = true
+set.nu = true
+set.noswapfile = true
+set.nobackup = true
+set.incsearch = true
+
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
+
+set.signcolumn = 'yes'
+
+set.updatetime = 50
